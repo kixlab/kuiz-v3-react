4598e87d799e1da71f0eb33d08808bb4751ca952
soyeongmin <ueaw@kaist.ac.kr>
2022-12-29 14:15:04 +0900

    Block Creating the Same Category

--- src/Components/CategoryInput/CategoryInput.tsx
+++ src/Components/CategoryInput/CategoryInput.tsx
@@ -3,18 +3,17 @@ import "./CategoryInput.scss";
 
 const CategoryInput = () => {
     const inputRef = useRef<string>("");
-    // const selectedCat = useRef<string>("");
     //추후에 DB로부터 받아올 예정..? 받아와야하나?
     const [catList, setCatList] = useState<string[]>(["Common misconception", "Form similar to answer"]);
     const [selectedCat, setSelectedCat] = useState<string>("");
 
     function addCategory() {
-        setCatList([inputRef.current, ...catList]);
+        if (!catList.includes(inputRef.current)) setCatList([inputRef.current, ...catList]);
+        else alert("You have the same category")
     }
     
     useEffect (() => {
         setSelectedCat(catList[0]);
-        // console.log(selectedCat.current);
     }, [catList])
 
     return (
@@ -23,20 +22,16 @@ const CategoryInput = () => {
                     <input onChange={(e) => inputRef.current = e.target.value}
                         id="CatInput"
                         type="text"
-                        placeholder='Select categories for your option or add your own'
-                    />
+                        placeholder='Select categories for your option or add your own'/>
                     <div onClick={addCategory} id="AddBtn">Add</div>
                 </div>
                 <div className='Categories'>
                     {catList.map((e,idx) => {
                         return (
                             <div key={idx} 
-                                onClick={() => {
-                                    setSelectedCat(e);
-                                }} 
-                                className={e===selectedCat ? 'CategoryAct' : 'Category'}>
-                                {e}
-                            </div>
+                                onClick={() => setSelectedCat(e)} 
+                                className={e===selectedCat ? 'CategoryAct' : 'Category'}
+                            >{e}</div>
                         )
                     })}
                 </div>
