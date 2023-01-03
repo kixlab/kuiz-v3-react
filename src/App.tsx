0c5f6061ae5c4242cadb01cd939df70484e1231d
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2023-01-03 17:47:14 +0900

    Setting up eslint + prettier (#2)
    * 📦 setup eslint and prettier
    
    * 🚨 fix type error

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import './App.scss';
-import Gnb from './Components/Gnb/Gnb';
-import LogIn from './Pages/LogIn/LogIn';
-import Enroll from './Pages/Enroll/Enroll';
-import MainPage from './Pages/MainPage/MainPage';
-import DetailAndCreateOption from './Pages/DetailAndCreateOption/DetailAndCreateOption';
-import CreateQuestion from './Pages/CreateQuestion/CreateQuestion';
-import MyPage from './Pages/MyPage/MyPage';
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Gnb from './Components/Gnb/Gnb'
+import CreateQuestion from './Pages/CreateQuestion/CreateQuestion'
+import DetailAndCreateOption from './Pages/DetailAndCreateOption/DetailAndCreateOption'
+import Enroll from './Pages/Enroll/Enroll'
+import LogIn from './Pages/LogIn/LogIn'
+import MainPage from './Pages/MainPage/MainPage'
+import MyPage from './Pages/MyPage/MyPage'
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
-  useEffect (() => {
-    console.log(isLoggedIn);
-  }, [isLoggedIn])
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true)
 
-  function login(state:boolean) {
-    setIsLoggedIn(!state);
-  }
+  useEffect(() => {
+    console.log(isLoggedIn)
+  }, [isLoggedIn])
 
   return (
     <div className="App">
       <Router>
-        <Gnb loginState={isLoggedIn}/>
-        <div className='Box'>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<LogIn isLoggedIn={isLoggedIn} login={login}/>} />
-          <Route path="/enroll" element={<Enroll />}/>
-          <Route path="/createQuestion" element={<CreateQuestion />}/>
-          <Route path="/question/createOption" element={<DetailAndCreateOption />}/>
-          <Route path="/mypage" element={<MyPage stemNum={3} optionNum={4}/>}/>
-        </Routes>
+        <Gnb loginState={isLoggedIn} />
+        <div className="Box">
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/enroll" element={<Enroll />} />
+            <Route path="/createQuestion" element={<CreateQuestion />} />
+            <Route path="/question/createOption" element={<DetailAndCreateOption />} />
+            <Route path="/mypage" element={<MyPage stemNum={3} optionNum={4} />} />
+          </Routes>
         </div>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
