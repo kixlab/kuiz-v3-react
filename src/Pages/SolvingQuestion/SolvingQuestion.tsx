4ee00011caa9ad5b34fc7b92ce4d5e716836591a
n4-t3 <nathanmekuria@gmail.com>
2023-01-04 15:18:29 +0900

    replaced styled-components with emotion/style

--- src/Pages/SolvingQuestion/SolvingQuestion.tsx
+++ src/Pages/SolvingQuestion/SolvingQuestion.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useState } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+import styled from '@emotion/styled';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useNavigate, useParams } from 'react-router-dom';
