79807ba0b29bb6958f1c67998c69128e3457c3a3
soyeongmin <ueaw@kaist.ac.kr>
2023-01-09 13:02:43 +0900

    Make Multiple category selectors

--- src/Components/CategoryInput.tsx
+++ src/Components/CategoryInput.tsx
@@ -1,117 +1,147 @@
 import { useEffect, useRef, useState } from 'react'
-import styled from '@emotion/styled';
+import styled from '@emotion/styled'
+import { css } from '@emotion/react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faX } from '@fortawesome/free-solid-svg-icons'
 
 interface Props {
-  getCategory: (categories: string) => void
+  getCategory: (cats: string[]) => void
 }
 
 export const CategoryInput = ({ getCategory }: Props) => {
   const inputRef = useRef<string>('')
-  //추후에 DB로부터 받아올 예정..? 받아와야하나?
+  //Should get category data from db
   const [catList, setCatList] = useState<string[]>(['Common misconception', 'Form similar to answer'])
-  const [selectedCat, setSelectedCat] = useState<string>('')
-  function addCategory() {
+  const [selections, setSelections] = useState<string[]>([])
+
+  function addElement() {
     if (!catList.includes(inputRef.current) && inputRef.current != '') setCatList([inputRef.current, ...catList])
     else if (inputRef.current == '') alert('Please enter the category') //no input case
     else alert('You have the same category') //duplicates case
   }
 
+  function removeElement(arr: string[], item: string) {
+    return arr.filter(e => e != item)
+  }
+
   useEffect(() => {
     //if the category list is updated, selected category will be changed
-    setSelectedCat(catList[0])
-    getCategory(catList[0])
-  }, [catList])
+    getCategory(selections)
+  }, [selections])
 
-    return (
-            <div>
-                <CatBox>+
-                    <CatInput style={{border: 'none'}} onChange={(e) => inputRef.current = e.target.value}
-                        type="text"
-                        placeholder='Select categories for your option or add your own'/>
-                    <AddBtn onClick={addCategory} >Add</AddBtn>
-                </CatBox>
-                <Categories>
-                    {catList.map((e,idx) => {
-                        return (
-                            <div key={idx} 
-                                onClick={() => {setSelectedCat(e); getCategory(e);}} 
-                            >
-                                {e===selectedCat ? <CategoryAct>{e}</CategoryAct>: <Category>{e}</Category>}
-                            </div>
-                        )
-                    })}
-                </Categories>
-            </div>
-    )
+  return (
+    <div>
+      <CatBox>
+        +
+        <CatInput
+          style={{ border: 'none' }}
+          onChange={e => (inputRef.current = e.target.value)}
+          type="text"
+          placeholder="Select categories for your option or add your own"
+        />
+        <AddBtn onClick={addElement}>Add</AddBtn>
+      </CatBox>
+      <Categories>
+        {catList.map((e, idx) => {
+          return (
+            <Category key={idx} selected={selections.includes(e)}>
+              <div
+                onClick={() => {
+                  if (selections.includes(e)) {
+                    setSelections(removeElement(selections, e))
+                  } else setSelections([...selections, e])
+                  getCategory(selections)
+                }}
+              >
+                {e}
+              </div>
+
+              <DeleteCat
+                onClick={() => {
+                  setCatList(removeElement(catList, e))
+                  setSelections(removeElement(selections, e))
+                }}
+              >
+                <FontAwesomeIcon icon={faX} />
+              </DeleteCat>
+            </Category>
+          )
+        })}
+      </Categories>
+    </div>
+  )
 }
 
-
 const CatBox = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    background-color: #e1e5eb;
-    margin-top: 12px;
-    padding: 4px 16px 4px 16px;
-    border-radius: 6px;
-    font-size: 16px;
-    font-weight: 500;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #e1e5eb;
+  margin-top: 12px;
+  padding: 4px 16px 4px 16px;
+  border-radius: 6px;
+  font-size: 16px;
+  font-weight: 500;
 `
 
 const CatInput = styled.input`
-    padding: 16px;
-    border-radius: 6px;
-    width: 100%;
-    box-sizing: border-box;
-    font-size: 16px;
+  padding: 16px;
+  border-radius: 6px;
+  width: 100%;
+  box-sizing: border-box;
+  font-size: 16px;
+  outline: none;
+  background: none;
+  margin: 0;
+  &:focus {
+    border: none;
     outline: none;
-    background: none;
-    margin: 0;
-    &:focus {
-        border:none;
-        outline:none;
-    }
+  }
 `
 
 const AddBtn = styled.div`
-    color: #3d8add;
-    border-bottom: 1px solid rgba(0,0,0,0);
-    cursor: pointer;
-    &:hover{
-        color: #2062aa;
-        border-color: #2062aa;
-    }
+  color: #3d8add;
+  border-bottom: 1px solid rgba(0, 0, 0, 0);
+  cursor: pointer;
+  &:hover {
+    color: #2062aa;
+    border-color: #2062aa;
+  }
 `
 
 const Categories = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    flex-direction: row;
-    gap: 6px;
-    font-size: 14px;
-    margin-top: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  gap: 6px;
+  font-size: 14px;
+  margin-top: 10px;
 `
 
-
-const Category = styled.div`
-    padding: 8px 16px 10px 16px;
-    border-radius: 20px;
-    background-color: #e1e5eb;
-    color: #212121;
-    cursor: pointer;
-    &:hover {
-        background-color: #ced8e4;
-    }
+const Category = styled.div<{ selected: boolean }>`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  padding: 0px 16px 0px 16px;
+  border-radius: 20px;
+  background-color: #e1e5eb;
+  color: #212121;
+  cursor: pointer;
+  &:hover {
+    background-color: #b6c7db;
+  }
+  ${props =>
+    props.selected &&
+    css`
+      background-color: #b6c7db;
+    `}
 `
-const CategoryAct = styled.div`
-    padding: 8px 16px 10px 16px;
-    border-radius: 20px;
-    background-color: #e1e5eb;
-    color: #212121;
-    cursor: pointer;
-    background-color: #b9c6d6;
-    &:hover {
-        background-color: #ced8e4;
-    }
+
+const DeleteCat = styled.div`
+  padding: 8px 0px 10px 0px;
+  color: #345478;
+  :hover {
+    color: white;
+  }
 `
