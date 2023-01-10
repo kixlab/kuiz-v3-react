7325217b452aa030da7e2f8bb758c2e883211d6d
n4-t3 <nathanmekuria@gmail.com>
2023-01-10 14:11:36 +0900

    integrated redux error state with popup

--- src/state/error/errorSlice.ts
+++ src/state/error/errorSlice.ts
@@ -2,11 +2,13 @@ import { createSlice } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
 
 export interface ErrorState {
+  error:boolean;
   title: string;
   message: string
 }
 
 const initialState: ErrorState = {
+  error:false,
   title: '',
   message: ''
 }
@@ -16,10 +18,12 @@ export const errorSlice = createSlice({
   initialState,
   reducers: {
     addError: (state, action: PayloadAction<[title:string,message:string]>) => {
+      state.error = true
       state.title = action.payload[0]
       state.message = action.payload[1]
     },
     removeError: (state) => {
+      state.error=false
       state.title = ''
       state.message = ''
     },
@@ -30,3 +34,7 @@ export const errorSlice = createSlice({
 export const { addError, removeError } = errorSlice.actions
 
 export default errorSlice.reducer
+
+// how to create error state
+// to create error use () => dispatch(addError(['title', 'message'])) from any page
+// example <input type="button" value="create error" onClick={() => dispatch(addError(['Logout Error', 'Please logout again there has been an error']))}/>
