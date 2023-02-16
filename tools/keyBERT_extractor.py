9bab426d58db8117d936274fcb2544a954a0109e
n4-t3 <nathanmekuria@gmail.com>
2023-02-16 18:11:11 +0900

    get data from external document

--- tools/keyBERT_extractor.py
+++ tools/keyBERT_extractor.py
@@ -1,4 +1,6 @@
 from keybert import KeyBERT
+import os 
+from PyPDF2 import PdfReader
 
 def extract_keywords(doc:str) -> list:
     """
@@ -17,9 +19,18 @@ def extract_key_phrases(doc:str) -> list:
     return key_phrases
 
 if __name__ == "__main__":
-    doc = """
-        To understand the notion of inertial reference frameWhich of the followings is an inertial referance frame? Understand the concept of Newton's first lawWhich of the following is true about Newton's first law? Understand the Newton's laws.Following questions are contain information about Newton's law. Please choose the wrong one. Understanding of frictional forceWhat is difference between static friction and kinetic friction? (quantitatively explain it) Application of Newton's 2nd LawAn object falls down from a frictionless ramp of a height of 10 m and reaches a horizontal plane with friction. The coefficient of kinetic friction of the plane is 0.4. How far can the object go before it comes to rest? (g=10m/s2) Learning about fluid resistance and the types of flow fluid hasWhat is the difference between laminar and turbulent flow? Understand what tension is and how to calculate the tension throughout a rope.Suppose you have a piece of rope. You hold the left end of the rope with your left hand and the right end of the rope with your right hand. You then pull the rope horizontally with 1 N force applied to the rope (horizontally) at each end. What is the tension at the midpoint of the rope? By the third Newton's 3rd law we know that every action has a reaction. It is very important to calculate the proper reaction force which is the negative of a sum of all acting forces.A box of mass 50 kg lies on a horizontal surface with a friction coefficient 0.4. A man is pulling the box at an angle 30 as shown in the figure below. What is the minimum required force to move the box? (g = 10 m/s^2) Newton's law and two dimensional equilibrium A child on a sled with a total mass of 35.0 kg, slides down the frictionless slope with an angle of 30 degrees. Calculate the child's (a) acceleration and (b) normal force (N). (g = 9.81 m/s^2) Speed of lightwhat is the speed of light? Speed of lightWhat is the speed of light? Alternating currentAlternating current is a type of current that is required for a transformer Understanding MeasurementWhich of the following units is NOT used for measuring length?
-    """
+    file = input("Please enter the name of the pdf file it should be in the current directory: ")
+    if file[-4:] != ".pdf":
+        file+=".pdf"
+    reader = PdfReader(file)
+    print(f"Your file has {len(reader.pages)} pages.")
+
+    doc =""
+    for page in reader.pages:
+        current_page = page
+        text = current_page.extract_text()
+        doc+=text
+    
     print("keywords: ")
     print(extract_keywords(doc))
     print("key phrases: ")
