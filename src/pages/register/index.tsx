c32f811634dcde361a66307340692445330ade0b
n4-t3 <nathanmekuria@gmail.com>
2023-03-07 23:11:21 +0900

    minor frontend updates

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -8,6 +8,8 @@ import { PutStudentIDParams, PutStudentIDResults } from '@api/insertStudentID'
 import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '@redux/store'
 import { addStudentID } from '@redux/features/userSlice'
+import { MOBILE_WIDTH_THRESHOLD } from 'src/constants/ui'
+import { palette, typography } from '@styles/theme'
 
 export default function StudentID() {
   const studentID = useSelector((state: RootState) => state.userInfo.studentID)
@@ -36,9 +38,11 @@ export default function StudentID() {
   }
   return (
     <CodeInputBox>
-      <strong>Please enter Your Student ID</strong>
-      <TextInput placeholder="Enter your student ID" onChange={inputChange} />
-      <FillBtn onClick={onSubmit}>Enter</FillBtn>
+      <Header>Please Enter Your Student ID</Header>
+      <InputSection>
+        <TextInput placeholder="Enter Student ID" onChange={inputChange} />
+        <FillBtn onClick={onSubmit}>Submit</FillBtn>
+      </InputSection>
     </CodeInputBox>
   )
 }
@@ -51,4 +55,42 @@ const CodeInputBox = styled.div`
   padding: 100px 60px 30px 60px;
   box-sizing: border-box;
   font-size: 18px;
+`
+
+const ClassButton = styled.button`
+  font-family: 'inter-r';
+  font-size: 15px;
+  padding: 16px;
+  border-radius: 6px;
+  background: ${palette.primary.light};
+  width: 100%;
+  box-sizing: border-box;
+  border: 2px solid transparent;
+  cursor: pointer;
+  &:hover {
+    border-color: ${palette.primary.main};
+  }
+`
+
+const IntroBox = styled.div`
+  border-radius: 8px;
+  background-color: white;
+  padding: 30px;
+  box-sizing: border-box;
+  margin: 24px 0;
+  @media (max-width: ${MOBILE_WIDTH_THRESHOLD}px) {
+    box-sizing: border-box;
+  }
+  ${typography.b02};
+`
+
+const Header = styled.div`
+  ${typography.hLabel};
+`
+
+const InputSection = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  column-gap: 12px;
 `
