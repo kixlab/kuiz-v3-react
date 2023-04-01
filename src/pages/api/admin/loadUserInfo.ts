373dd1dbe76b0be1b81d3fbf980706e356e7b962
n4-t3 <nathanmekuria@gmail.com>
2023-04-01 16:26:27 +0900

    removed succes field and unnecessary userclass call

--- src/pages/api/admin/loadUserInfo.ts
+++ src/pages/api/admin/loadUserInfo.ts
@@ -1,7 +1,6 @@
 import { ClassModel } from '@server/db/class'
 import { User, UserModel } from '@server/db/user'
 import { apiController } from '@utils/api'
-import { Types } from 'mongoose'
 
 export interface LoadUserInfoParams {
   cid: string
@@ -10,19 +9,16 @@ export interface LoadUserInfoParams {
 export interface LoadUserInfoResults {
   className: string
   students: User[]
-  success: boolean
 }
 
 export default apiController<LoadUserInfoParams, LoadUserInfoResults>(async ({ cid }) => {
   const userClass = await ClassModel.findById(cid)
   if (userClass) {
     const students = await UserModel.find({ _id: { $in: userClass.students } })
-    const courseClass = await ClassModel.findById(new Types.ObjectId(cid))
-    const className = courseClass.name
+    const className = userClass.name
     return {
       className,
       students,
-      success: true,
     }
   }
   throw new Error('Class not found')
