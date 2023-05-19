1e8497ce110e7ec93a9448b0d67ca651265104af
Nathan <112761202+n4-t3@users.noreply.github.com>
2023-05-19 20:15:14 +0900

    created filteration based on topic for mypage (#83)

--- src/pages/api/loadCreatedStemData.ts
+++ src/pages/api/loadCreatedStemData.ts
@@ -1,15 +1,27 @@
 import { QStem } from '@server/db/qstem'
 import { apiController } from '@utils/api'
+import { ID } from 'src/types/common'
 
-export interface LoadCreatedStemDataParams {}
+export interface LoadCreatedStemDataParams {
+  cid: ID
+  topic: string | undefined
+}
 
 export interface LoadCreatedStemDataResults {
   madeStem: QStem[]
 }
 
-export default apiController<LoadCreatedStemDataParams, LoadCreatedStemDataResults>(async ({}, user) => {
+export default apiController<LoadCreatedStemDataParams, LoadCreatedStemDataResults>(async ({ cid, topic }, user) => {
   const userAfterPopulation = await user.populate<{ made: QStem[] }>('made')
+  const filteredQStem = userAfterPopulation.made.filter(qStem => {
+    if (topic) {
+      const regex = new RegExp(`\\b${topic}\\b`)
+      return qStem.class.toString() === cid && regex.test(qStem.learningObjective)
+    } else {
+      return qStem.class.toString() === cid
+    }
+  })
   return {
-    madeStem: userAfterPopulation.made,
+    madeStem: filteredQStem,
   }
 })
