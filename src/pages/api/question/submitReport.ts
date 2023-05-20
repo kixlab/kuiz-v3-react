c5519f2625bcf9ff09d7a1b9ed0b2abf8c4cde27
n4-t3 <nathanmekuria@gmail.com>
2023-05-20 19:29:58 +0900

    removed console log

--- src/pages/api/question/submitReport.ts
+++ src/pages/api/question/submitReport.ts
@@ -13,7 +13,6 @@ export interface SubmitReportResults {}
 
 export default apiController<SubmitReportParams, SubmitReportResults>(async ({ qid, comment }, user) => {
   const report = await reportService.create(new Types.ObjectId(user.id), new Types.ObjectId(qid), comment)
-  console.log(report)
   if (report) {
     await QStemModel.updateOne({ _id: qid }, { $push: { report: report._id } })
   }
